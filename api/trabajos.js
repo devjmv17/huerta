951584a727a9d7bb03410ba821d84c7c35b7eb03
Juan Manuel Vivancos <devjmv17@gmail.com>
2025-06-30 18:07:14 +0200

    Trabajos edit

--- api/trabajos.js
+++ api/trabajos.js
@@ -8,18 +8,18 @@ module.exports = async (req, res) => {
   }
   
   if (req.method === 'POST') {
-    const { id, tareaId, personaId, huertoId, fecha, horas, notas } = req.body;
+    const { id, tareaid, personaid, huertoid, fecha, horas, notas } = req.body;
     const idNum = Number(id);
     if (idNum > 0) {
       const { rows } = await pool.query(
-        'UPDATE trabajos SET tareaId = $1, personaId = $2, huertoId = $3, fecha = $4, horas = $5, notas = $6 WHERE id = $7 RETURNING *',
-        [tareaId, personaId, huertoId, fecha, horas, notas, idNum]
+        'UPDATE trabajos SET tareaid = $1, personaid = $2, huertoid = $3, fecha = $4, horas = $5, notas = $6 WHERE id = $7 RETURNING *',
+        [tareaid, personaid, huertoid, fecha, horas, notas, idNum]
       );
       return res.status(200).json(rows[0]);
     } else {
       const { rows } = await pool.query(
-        'INSERT INTO trabajos (tareaId, personaId, huertoId, fecha, horas, notas) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
-        [tareaId, personaId, huertoId, fecha, horas, notas]
+        'INSERT INTO trabajos (tareaid, personaid, huertoid, fecha, horas, notas) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+        [tareaid, personaid, huertoid, fecha, horas, notas]
       );
       return res.status(201).json(rows[0]);
     }
