f6c93b3818ca5dcda43aa27af9be36ed867282d8
Juan Manuel Vivancos <devjmv17@gmail.com>
2025-07-11 18:57:23 +0200

    Creación Login 6

--- api/crear_admin.js
+++
@@ -1,7 +0,0 @@
-const { crearUsuario } = require('./db');
-
-(async () => {
-  const usuario = await crearUsuario('admin', '2020', 'admin');
-  console.log('Usuario admin creado:', usuario);
-  process.exit();
-})();
